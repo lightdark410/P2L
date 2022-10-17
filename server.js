e0d26f07f8a8959fdd4766feb17d0a21c1d90ed2
Kisa Hölzl <kisa@kisatan.org>
2022-10-17 11:49:29 +0200

    add proper session store

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const config = require("config");
 const express = require("express");
-
 const PORT = process.env.PORT || 8090;
 const app = express();
 const session = require("express-session");
+const mysql = require("mysql2/promise");
+const MySQLStore = require("express-mysql-session")(session);
 
 const mainController = require("./controllers/mainController");
 const apiController = require("./controllers/apiController");
@@ -10,8 +12,17 @@ const apiController = require("./controllers/apiController");
 app.set("view engine", "ejs");
 
 app.use("/assets", express.static("assets"));
+
+const connectionPool = mysql.createPool(config.get("dbConfig"));
+const sessionStore = new MySQLStore(
+  {
+    expiration: 43200000, // expire sessions after 12 hours
+  },
+  connectionPool
+);
 app.use(
   session({
+    store: sessionStore,
     secret: "secret",
     resave: true,
     saveUninitialized: true,
@@ -36,6 +47,7 @@ server = app.listen(PORT, () => {
 process.on("SIGTERM", () => {
   console.log("Received SIGTERM: closing HTTP server.");
   // TODO: close DB connection pool
+  sessionStore.close();
   server.close(() => {
     console.log("HTTP server closed");
   });
