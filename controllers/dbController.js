84208aefc566a584f7a9336d47b462f1082bcb7d
Kisa Hölzl <kisa@kisatan.org>
2022-11-23 10:53:40 +0100

    fix typos

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -36,6 +36,71 @@ const CTE_getFullLocationPathFromLeaf_single = function () {
 	  SELECT fullpath FROM cte WHERE parent = 0;`;
 };
 
+const cleanUpConnection = async function (connection) {
+  await connection.rollback();
+  await connection.release();
+};
+
+const reorderPlaces = async function (oldPlaces, connection) {
+  let createdConnection = false;
+  if (connection === undefined) {
+    connection = connPool.getConnection();
+    connection.beginTransaction();
+    createdConnection = true;
+  }
+  const result = {};
+  result.changed = 0;
+  const emptyPlaces = [];
+  const occupiedPlaces = [];
+  for (const place of oldPlaces) {
+    if (place.stock_id === null) {
+      emtpyPlaces.push(place);
+    } else {
+      occupiedPlaces.push(place);
+    }
+  }
+  // we start the counter at zero since we increment at the start of each iteration
+  let counter = 0;
+  try {
+    for (const place of occupiedPlaces) {
+      counter++;
+      if (place.place !== counter) {
+        await connection.query(
+          `UPDATE storage_place
+           SET place = ?
+           WHERE id = ?`,
+          [counter, place.id]
+        );
+        result.changed++;
+      }
+    }
+    result.occupied = counter;
+    for (const place of emptyPlaces) {
+      counter++;
+      if (place.place !== counter) {
+        await connection.query(
+          `UPDATE storage_place
+           SET place = ?
+           WHERE id = ?`,
+          [counter, place.id]
+        );
+        result.changed++;
+      }
+    }
+    result.total = counter;
+  } catch (error) {
+    if (createdConnection) {
+      cleanUpConnection(connection);
+    }
+    throw error;
+  }
+  if (createdConnection) {
+    connection.commit();
+    connection.release();
+  }
+  return result;
+};
+
 /*********************************************************************
  *                      EXPORTED FUNCTIONS                           *
  *********************************************************************/
@@ -50,8 +115,7 @@ async function deleteTask(taskID) {
     await connection.query("DELETE FROM task_log WHERE task_id = ?", [taskID]);
     await connection.query("DELETE FROM task WHERE id = ?", [taskID]);
   } catch (error) {
-    await connection.rollback();
-    await connection.release();
+    cleanUpConnection(connection);
     throw error;
   }
   await connection.commit();
@@ -59,7 +123,7 @@ async function deleteTask(taskID) {
 }
 
 // FIXME: don't log a change if the actual change amount is 0
-// TODO: move logging, tasklog and updating stock values to updateTaskStatus()
+// TODO: move logging, tasklog and updating stock values to updateTaskEntryAmount()
 async function finishTask(taskID, username) {
   const connection = await connPool.getConnection();
   await connection.beginTransaction();
@@ -73,8 +137,7 @@ async function finishTask(taskID, username) {
     );
     if (taskInfo[0].status === 1) {
       // if task was alreay finished: abort
-      await connection.rollback();
-      await connection.release();
+      cleanUpConnection(connection);
       return "Already finished";
     }
     const [taskEntries] = await connection.query(
@@ -83,7 +146,7 @@ async function finishTask(taskID, username) {
        WHERE task_id = ?`,
       [taskID]
     );
-    for (entry of taskEntries) {
+    for (const entry of taskEntries) {
       const [stockEntry] = await connection.query(
         `SELECT
            article.name,
@@ -196,8 +259,7 @@ async function finishTask(taskID, username) {
     }
   } catch (error) {
     // if anything goes wrong abort the transaction and rethrow the error for the caller to handle
-    connection.rollback();
-    connection.release();
+    cleanUpConnection(connection);
     throw error;
   }
   connection.commit();
@@ -211,6 +273,81 @@ async function getStockIDByArticlenumber(articlenumber) {
     [articlenumber]
   );
   return rows;
+}
+
+async function resizeStorageLocation(storageLocationID, newSize) {
+  const connection = await connPool.getConnection();
+  await connection.beginTransaction();
+  const result = {};
+  try {
+    const [oldData] = await connection.query(
+      `SELECT id, places
+       FROM storage_location
+       WHERE id = ?
+       FOR UPDATE`,
+      [storageLocationID]
+    );
+    if (oldData.length === 0) {
+      await cleanUpConnection(connection);
+      return { error: "ERR_LOC_NOT_FOUND" };
+    }
+    result.oldSize = oldData[0].places;
+    result.newSize = newSize;
+    const [oldPlaces] = await connection.query(
+      `SELECT *
+       FROM storage_place
+       WHERE storage_location_id = ?
+       FOR UPDATE`,
+      [storageLocationID]
+    );
+    const reorderedPlacesCount = await reorderPlaces(oldPlaces, connection);
+    result.occupied = reorderedPlacesCount.occupied;
+    result.reordered = reorderedPlacesCount.changed;
+    if (reorderedPlacesCount.occupied > newSize) {
+      await cleanUpConnection(connection);
+      return { error: "ERR_NOT_ENOUGH_EMPTY_PLACES" };
+    }
+    if (
+      reorderedPlacesCount.total === newSize &&
+      newSize === oldData[0].places &&
+      reorderedPlacesCount.changed === 0
+    ) {
+      await cleanUpConnection(connection);
+      return { error: "ERR_NOTHING_TO_DO" };
+    }
+    if (reorderedPlacesCount.total > newSize) {
+      await connection.query(
+        `DELETE
+         FROM storage_place
+         WHERE storage_location_id = ?
+           AND place > ?`,
+        [storageLocationID, newSize]
+      );
+    } else if (reorderedPlacesCount.total < newSize) {
+      const newPlacesArr = [];
+      for (let i = reorderedPlacesCount.total; i <= newSize; i++) {
+        newPlacesArr.push([storageLocationID, i]);
+      }
+      await connection.query(
+        `INSERT
+         INTO storage_place (storage_location_id, place)
+         VALUES ?`,
+        [newPlacesArr]
+      );
+    }
+    await connection.query(
+      `UPDATE storage_location
+       SET places = ?
+       WHERE id = ?`,
+      [newSize, storageLocationID]
+    );
+  } catch (error) {
+    await cleanUpConnection(connection);
+    throw error;
+  }
+  await connection.commit();
+  await connection.release();
+  return result;
 }
 
 async function updateTaskEntryAmount(taskID, stockID, amountReal) {
@@ -239,8 +376,7 @@ async function updateTaskEntryAmount(taskID, stockID, amountReal) {
       [amountReal, result.newStatus, oldData[0].id]
     );
   } catch (error) {
-    await connection.rollback();
-    await connection.release();
+    cleanUpConnection(connection);
     throw error;
   }
   await connection.commit();
@@ -260,6 +396,7 @@ module.exports = {
   deleteTask,
   finishTask,
   getStockIDByArticlenumber,
+  resizeStorageLocation,
   updateTaskEntryAmount,
   updateTaskStatus,
 };
